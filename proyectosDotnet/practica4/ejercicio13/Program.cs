5d7da2f6ae6c2b6d1a664655b6a4e52053dc3c7a
NeoG14 <nicolaplatajob@gmail.com>
2023-04-15 22:11:45 -0300

    Ejercicio 13

--- proyectosDotnet/practica4/ejercicio13/Program.cs
+++ proyectosDotnet/practica4/ejercicio13/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//13) Reemplazar estas líneas de código por otras equivalentes que utilicen el operador null-coalescing (??) y/o la asignación null-coalescing (??=)
+
+string? st = "";
+string? st1 = null;
+string? st2 = null;
+string? st3 = null;
+string? st4 = null;
+
+if (st1 == null)
+{
+    if (st2 == null)
+    {
+        st = st3;
+    }
+    else
+    {
+        st = st2;
+    }
+}
+else
+{
+    st = st1;
+}
+
+
+
+if (st4 == null)
+{
+    st4 = "valor por defecto";
+}
+
+
+st = st1 ?? st3 ?? st2 ?? st1;
+st4 ??= "valor por defecto";
+
+if(st==null)
+    Console.WriteLine("NULL");
+else
+    Console.WriteLine(st4);
+
+Console.ReadLine();
+
+
+
+
+
